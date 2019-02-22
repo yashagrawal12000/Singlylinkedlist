1b1f536acf840af111f8d64422802f9f5471a833
yashagrawal12000 <yash.agrawal_cs17@gla.ac.in>
2019-02-22 09:34:53 +0530

    implement method

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -5,6 +5,32 @@ public class SinglyLinkedList {
     private Node head = null;
     private int size = 0;
 
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("{");
+        Node temp = this.head;
+
+        while (temp.next != null) {
+            result.append(temp.data);
+            temp = temp.next;
+        }
+        result.append("}");
+
+        return result.toString();
+
+    }
+
+    public static void main(String[] args) {
+
+        SinglyLinkedList linkedlist = new SinglyLinkedList();
+
+        System.out.println(linkedlist);
+
+
+    }
+
+
         private static  class Node{
             private int data;
             private Node next;
